6ab8bd0ddda931b83baec6052ede1f8f4f32ff32
Bagna Farouk Omar <91009147+omarbagna@users.noreply.github.com>
2021-10-24 10:44:15 +0000

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,45 +1,117 @@
-//Global option 
+Chart.defaults.font.size = 16;
+Chart.defaults.color = '#4D1ED3';
+
 // setup
 const data = {
-    labels:['Boston', 'Worcester', 'Springfield', 'Lowell', 'Campbridge', 'New Bedford'],
+    labels:['Accra', 'Kumasi', 'Tamale', 'Wa', 'Cape Coast', 'Tema'],
     datasets:[{
-        label: 'Population',
+        label: 'Sample Data',
         data: [
-            617594,
-            1811045,
-            153060,
-            106519,
-            105162,
-            95072
+            6175,
+            4811,
+            8530,
+            5065,
+            3051,
+            9507
         ],
         borderColor: '#72ebba',
-        tension: 0.5
+        tension: 0.3
     }]
 };
 
-// config
-const config = {
+
+// TCH congestion config
+const tchConfig = {
     type: 'line',
     data,
     options: {
         scales: {
-         y:{ticks:{
-            color:'#4D1ED3'
-         },
-            beginAtZero: true,
-            },
-         x:{ticks:{
-            color:'#4D1ED3'
-            },
+            y: {
                 beginAtZero: true,
-         }
+                },
 
+            x: {
+                beginAtZero: true,
+                }
+            }
         }
-    }
-};
+    };
 
-// init /render block
-const myChart = new Chart(
+
+// TCH congestion config
+const sdcchConfig = {
+    type: 'bar',
+    data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                },
+
+            x: {
+                beginAtZero: true,
+                }
+            }
+        }
+    };
+
+
+// TCH congestion config
+const callDropConfig = {
+    type: 'line',
+    data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                },
+
+            x: {
+                beginAtZero: true,
+                }
+            }
+        }
+    };
+
+
+// TCH congestion config
+const callConnectConfig = {
+    type: 'bar',
+    data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                },
+
+            x: {
+                beginAtZero: true,
+                }
+            }
+        }
+    };
+
+
+// render tch graph
+const tchChart = new Chart(
     document.getElementById('tch-graph'),
-    config
-);
+    tchConfig
+    );
+
+// render sdcch graph
+const sdcchChart = new Chart(
+    document.getElementById('sdcch-graph'),
+    sdcchConfig
+    );
+
+// render call drop graph
+const callDropChart = new Chart(
+    document.getElementById('callDrop-graph'),
+    callDropConfig
+    );
+
+// render call connection graph
+const callConnectChart = new Chart(
+    document.getElementById('callConnect-graph'),
+    callConnectConfig
+    );
